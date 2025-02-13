a338ff8342aba385f7e356366eda275decebb941
Michaevl6 <michael.canales@cobraperu.com.pe>
2025-02-13 01:04:17 -0500

    cambios

--- src/core/infrastructure/database/postgres.config.ts
+++ src/core/infrastructure/database/postgres.config.ts
@@ -2,15 +2,16 @@
 import { DataSource } from "typeorm";
 import { User } from "../../../modules/users/domain/User.entity"
 
-const { PG_DATABASE, PG_PASSWORD, PG_USERNAME } = process.env;
+const { PG_DATABASE, PG_PASSWORD, PG_USERNAME, PG_HOST, PG_PORT } = process.env;
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
+  host: PG_HOST,
+  port: Number(PG_PORT),
   username: PG_USERNAME,
   password: PG_PASSWORD,
   database: PG_DATABASE,
   synchronize: true,
+  ssl: true,
   entities: [User],
   logging: false // borrar la consola que pone en la terminal
 });
